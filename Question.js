bba0712602c427d05abb7b6e66e655544f3fdb38
danpq14 <61695301+danpq14@users.noreply.github.com>
2020-04-01 11:26:03 +0700

    final1

--- Question.js
+++ Question.js
@@ -1,9 +1,26 @@
 let q = [];
 q[0] = {
     correct : 'BAOCAO',
-    diachi : 'cau1.png'
+    diachi : 'cau1.png',
+    hint : 'Một công việc mà nhân viên thường phải làm với cấp trên'
 };
 q[1] = {
     correct : 'CAUMAY',
-    diachi : 'cau2.jpg'
+    diachi : 'cau2.jpg',
+    hint : 'Một môn thể thao mà Việt Nam là cường quốc'
+};
+q[2] = {
+    correct : 'CUNGCAU',
+    diachi : 'cau3.png',
+    hint : 'Một khái niệm trong kinh tế học'
+};
+q[3] = {
+    correct : 'BAHOA',
+    diachi : 'cau4.png',
+    hint : 'Từ dùng để chỉ những người nói chuyện không thực tế'
+};
+q[4] = {
+    correct : 'CADAO',
+    diachi : 'cau5.png',
+    hint : '... tục ngữ'
 };
--- game.js
+++ game.js
@@ -1,6 +1,30 @@
 //kHai báo biến
 let level = 0;
 let dapAn;
+let point = 1000;
+let pointHint = 1000;
+
+//-------Level and Point---------//
+function levelAndPoint() {
+    switch (level) {
+        case 0 :
+            point = 1000;
+            break;
+        case 1 :
+            point = 2000;
+            break;
+        case 2 :
+            point = 4000;
+            break;
+        case 3 :
+            point = 8000;
+            break;
+        case 4 :
+            point = 12000;
+            break;
+    }
+}
+
 
 //----------drawQuestion---------//
 function draw() {
@@ -16,6 +40,7 @@ function fill(text) {
 //Clear
 function clear() {
     document.getElementById('answer').value = '';
+    document.getElementById('result').innerHTML = '';
 }
 
 
@@ -30,11 +55,25 @@ function check() {
 }
 
 
+//--------Hint---------//
+function hint() {
+    document.getElementById('result').innerHTML = q[level].hint;
+    point = point - pointHint;
+    document.getElementById('point').innerHTML ='<b>' + 'Điểm thưởng : ' + point + '</b>';
+}
+
+
 //----------Next----------//
 function next() {
     if (dapAn === q[level].correct) {
         level++;
         draw();
         clear();
+        levelAndPoint();
+        pointHint *= 1.5;
+        document.getElementById('level').innerHTML ='<b>' +'Level : ' + (level+1)+'</b>';
+        document.getElementById('point').innerHTML ='<b>'+ 'Điểm thưởng : ' + point + '</b>';
+    } else {
+        document.getElementById('result').innerHTML = 'Bạn chưa trả lời chính xác';
     }
 }
