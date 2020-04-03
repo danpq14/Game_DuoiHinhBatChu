8789fc90af9504f3a2c127c37ebf9a6c443525c3
danpq14 <61695301+danpq14@users.noreply.github.com>
2020-04-03 12:12:36 +0700

    them sound effect

--- game.js
+++ game.js
@@ -3,6 +3,8 @@ let level = 0;
 let dapAn;
 let point = 1000;
 let pointHint = 1000;
+let sound1 = document.getElementById('audio1');
+let sound2 = document.getElementById('audio2');
 
 //----------drawQuestion---------//
 function draw() {
@@ -35,12 +37,14 @@ function next() {
     if (dapAn === q[level].correct) {
         level++;
         point += 2000;
+        sound1.play();
         draw();
         clear();
         pointHint = Math.floor(pointHint*1.2);
         document.getElementById('level').innerHTML ='<b>' +'Level : ' + (level+1)+'</b>';
         document.getElementById('point').innerHTML ='<b>'+ 'Điểm thưởng : ' + point + '</b>';
     } else {
+        sound2.play();
         document.getElementById('result').innerHTML = 'Bạn chưa trả lời chính xác';
     }
 }
